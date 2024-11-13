76b67e842dfb24ef11b0737092c1b192f3926dd7
Jeevan Kumar <jeevankumar3229@gmail.com>
2024-11-13 16:19:18 +0300

    modify app.js

--- app.js
+++ app.js
@@ -10,11 +10,5 @@ app.use(bodyParser.json());
 app.use("/auth", authRoutes);
 app.use("/tasks", taskRoutes);
 
-// Error handling middleware
-app.use((err, req, res, next) => {
-  console.error(err.stack);
-  res.status(500).json({ error: "Something went wrong!" });
-});
-
 const PORT = process.env.PORT || 3000;
 app.listen(PORT, () => console.log(`Server running on port ${PORT}`));
